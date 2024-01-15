4688417ba95609b726f4ee2ad556137c070d2fdf
Ankit Aher <115979078+ANKITAHER@users.noreply.github.com>
2024-01-15 17:31:17 +0000

    arrays basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,25 @@
+const myArrays = [ 1,2,23,4,5,6,]
+const myHeros = [ 'ankit' , 'ajinkya', 'kapil']
+const myArraysTwo = new Array(1,234,4)
+
+// console.log (myArrays[2])
+// myArrays.push(8)
+// myArrays.pop()
+// myArrays.unshift(5)
+// console.log (myArrays)
+// console.log (myArrays.includes(9))
+// console.log (myArrays.indexOf(6))
+
+
+// const array = myArrays.join()
+// console.log (myArrays)
+// console.log(array)
+
+console.log("a" , myArrays)
+
+const myn1 = myArrays.slice(1,3)
+console.log("b" , myn1)
+
+
+const myn2 =  myArrays.splice(1,3)
+console.log("c", myn2)
