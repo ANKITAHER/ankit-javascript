f9e4bef8400b431c83b5524906a3b2cda14356a1
Ankit Aher <115979078+ANKITAHER@users.noreply.github.com>
2024-01-15 16:17:32 +0000

    number and maths

---
+++ 01_basics/06_number_and_maths.js
@@ -0,0 +1,35 @@
+// const number = 400
+// console.log(number)
+
+
+// const age = new Number(400)
+// console.log(age)
+
+// console.log(age.toString().length)
+
+// console.log(age.toFixed(1))
+
+
+
+// const otherNumber = 165.5168
+
+// console.log(otherNumber.toPrecision(4))
+
+
+//***************************************Maths******************************************************** 
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(5.255));
+console.log(Math.floor(5.255));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1)
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+
+console.log (Math.floor(Math.random()* (max-min+1)) +min )
