efcdae7db5f9d8274d0c8390734d0432396f1d36
Ankit Aher <115979078+ANKITAHER@users.noreply.github.com>
2024-01-15 08:43:42 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -10,10 +10,35 @@ console.log (valueInNumber )
 
 
 let isLogIn = 1
-
 let booleanIsLogin = Boolean(isLogIn)
 console.log(  booleanIsLogin)
 
 // if 1  =  true  if  ''  = false 
 
+let someNmuber = 33
+let stringNmuber = String(someNmuber)
+console.log(typeof stringNmuber)
 
+
+// ****************************** OPERATIONS ******************************
+let value = 3
+let negative = -value
+console.log(negative)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+
+
+console.log("1" + 2)
+console.log( 2 + "1" )
+console.log("1" + 2 + 2)
+console.log( 2 + 2 + "1")
+console.log((3+4) * 2 % 3)
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
