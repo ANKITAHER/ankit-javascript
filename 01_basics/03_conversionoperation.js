0df38c540c6cab3c8b13957323d0688465d24cfe
Ankit Aher <115979078+ANKITAHER@users.noreply.github.com>
2024-01-15 08:16:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,19 @@
+let score = "123sds"
+
+console.log(typeof score);
+console.log (typeof(score))
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log (valueInNumber )
+
+
+let isLogIn = 1
+
+let booleanIsLogin = Boolean(isLogIn)
+console.log(  booleanIsLogin)
+
+// if 1  =  true  if  ''  = false 
+
+
