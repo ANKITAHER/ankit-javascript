49d5aac5d377c891ac21fb7cdd3d7c69b2bf13bf
Ankit Aher <115979078+ANKITAHER@users.noreply.github.com>
2024-01-15 07:35:03 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,21 @@
+let name = "ankit"
+let age = 23
+let isLogIn = false
+let state;
+
+
+
+// primitive datatype
+
+//number => 2 to power  53
+//bigint
+//string => ""
+//boolean = > true/false
+// undefined  =>
+//symbol => unique
+
+
+// non primitive datatype 
+
+console.log(typeof undefined); // undefined 
+ console.log(typeof null); //object
