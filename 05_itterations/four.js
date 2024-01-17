996b1880294e2355fa9de871651acaf0122962b2
Ankit Aher <115979078+ANKITAHER@users.noreply.github.com>
2024-01-17 16:30:19 +0000

    for in

---
+++ 05_itterations/four.js
@@ -0,0 +1,24 @@
+const myobject = {
+    js : "javascript",
+    c : "c plus",
+    ios: "swift",
+    rb: "ruby"
+}
+
+// for (const key in object) {
+//    console.log(key);
+// }
+
+
+for (const key in myobject) {
+    // console.log(`${key} ,  is  the shortcut for ${myobject[key]}`);
+
+ }
+
+
+ const arr = ['ankit','kapil','ajinkya']
+
+ for (const key in arr) {
+    // console.log(arr[key])
+ }
+
